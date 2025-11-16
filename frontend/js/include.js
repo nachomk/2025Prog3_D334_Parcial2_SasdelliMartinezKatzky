446d508df4250eaa8cb41ae80378a683bcf397fb
Nacho <nacho.mk@hotmail.com>
2025-11-16 15:43:00 -0300

    IMK001 - Modales de detalles y logica de salir y entrar con Nombre

--- frontend/js/include.js
+++ frontend/js/include.js
@@ -3,3 +3,19 @@ document.querySelectorAll('[data-include]').forEach(async (el) => {
   const html = await fetch(url).then(r => r.text());
   el.outerHTML = html;
 });
+
+// Logica que agrega el nombre ingresado en welcome en el saludo de la HOME
+setTimeout(() => { // se agrego un timeout para agregarle prolijidad a la hora de querer salir y volver al login inicial
+  const nombre = localStorage.getItem('nombre');
+  if (nombre) {
+    document.getElementById('hm-bienvenida').textContent = `Bienvenido, ${nombre}. Elija su servicio`;
+  }
+
+  const botonSalir = document.getElementById('hd-salir');
+  if (botonSalir) {
+    botonSalir.addEventListener('click', function () {
+      localStorage.removeItem('nombre');
+      window.location.href = '01_welcome.html';
+    });
+  }
+}, 100);
---
+++ frontend/js/modals.js
@@ -0,0 +1,24 @@
+// Abrir modal
+document.querySelectorAll('.link').forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault(); // evita el salto de ancla
+    const modal = document.querySelector(link.getAttribute('href'));
+    modal.classList.add('show');
+  });
+});
+
+// Cerrar modal con la X
+document.querySelectorAll('.modal .close').forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.closest('.modal').classList.remove('show');
+  });
+});
+
+// Cerrar clickeando afuera
+document.querySelectorAll('.modal').forEach(modal => {
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      modal.classList.remove('show');
+    }
+  });
+});
